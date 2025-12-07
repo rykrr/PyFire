048ebe24cf52511b19ff144e559d601a580d2735
Ryan K. <ryan@rykr.ca>
2025-12-06 23:41:22 -0500

    Add files via upload

--- pyfire.py
+++ pyfire.py
@@ -1,25 +1,28 @@
-## 
-##   8888888b.            .d888 d8b                  
-##   888   Y88b          d88P"  Y8P                  
-##   888    888          888                         
-##   888   d88P 888  888 888888 888 888d888  .d88b.  
-##   8888888P"  888  888 888    888 888P"   d8P  Y8b 
-##   888        888  888 888    888 888     88888888 
-##   888        Y88b 888 888    888 888     Y8b.     
-##   888         "Y88888 888    888 888      "Y8888  
-##                   888                             
-##              Y8b d88P           
+##
+##
+##   8888888b.            .d888 d8b
+##   888   Y88b          d88P"  Y8P
+##   888    888          888
+##   888   d88P 888  888 888888 888 888d888  .d88b.
+##   8888888P"  888  888 888    888 888P"   d8P  Y8b
+##   888        888  888 888    888 888     88888888
+##   888        Y88b 888 888    888 888     Y8b.
+##   888         "Y88888 888    888 888      "Y8888
+##                   888
+##              Y8b d88P
 ##               "Y88P"             github.com/rykrr
-## 
+##
 ################################################################################
 ################################################################################
 
 from argparse import ArgumentParser
 from time import sleep
 import asyncio
+import time
 import os
 
 from scipy.signal import convolve2d as conv2d
+from numpy.random import rand
 import numpy as np
 
 
@@ -71,7 +74,13 @@ arg_parser.add_argument(
     "--margin", type=int, default=5, help="Margins from side"
 )
 arg_parser.add_argument(
-    "--compat", action="store_true", help="Force compatibility set for vTTY"
+    "--mode", type=str, default="fade", help="Choose charset"
+)
+arg_parser.add_argument(
+    "--buffer", type=int, default=10, help="Render buffer size"
+)
+arg_parser.add_argument(
+    "--cache", type=int, default=0, help="Use a fixed set of ignitions"
 )
 args = arg_parser.parse_args()
 
@@ -97,14 +106,21 @@ RST = "\x1b[0m"
 # Style settings
 ################################################################################
 
-CHARS = "0123456789A"
-CHARS = " .,;:!!!!!!"
-CHARS = " ░▒▓███████"
-CHARS = " ░░░░░░░░░░"
+CHARSETS = {
+    "numbers": "0123456789",
+    "dither": " ░░░░░░░░░",
+    "fade": " ░▒▓██████",
+    "chars": " .,;:!!!!!",
+    "compat": " ░▒▓█▓▒░██",
+}
+
+if args.mode not in CHARSETS:
+    raise KeyError(f"{args.mode} not foun. Available: {CHARSETS.keys()}")
+
+CHARS = CHARSETS[args.mode]
 COLOURS = [0, 196, 202, 208, 214, 226, 226, 226, 226, 226, 226]
 
-if args.compat:
-    CHARS = " ░▒▓█▓▒░███"
+if args.mode == "compat":
     COLOURS = [0, 196, 196, 214, 214, 214, 214, 214, 226, 226, 226]
 
 CHARS = [ESC.format(colour) + char for colour, char in zip(COLOURS, CHARS)]
@@ -127,13 +143,25 @@ KERNEL = np.array(
 
 KERNEL = (KERNEL / KERNEL.sum()) * args.alpha
 
-# Kernel / Filter
+
+# Main
 ################################################################################
 
 
-async def framegen(queue: asyncio.Queue):
+async def generate(queue: asyncio.Queue):
 
     heatmap = np.zeros((HEIGHT, WIDTH))
+
+    ignitions = None
+    hashes = {}
+    cache = []
+
+    start_time = time.perf_counter()
+
+    if args.cache:
+        # cache = np.zeros((int(args.cache * 1.5), HEIGHT, WIDTH))
+        seeds = args.strength * abs(rand(args.cache, WIDTH - (2 * MARGIN)))
+        seed_idx = 0
 
     while True:
         heatmap[:-1] = conv2d(
@@ -145,24 +173,51 @@ async def framegen(queue: asyncio.Queue):
             args.epsilon * heatmap * np.tanh(args.delta * heatmap)
         )
 
-        row = args.strength * abs(np.random.rand(1, WIDTH - (2 * MARGIN)))
-        heatmap[-1][MARGIN:-MARGIN] = row
+        if not args.cache:
+            seed = args.strength * abs(rand(1, WIDTH - (2 * MARGIN)))
+        else:
+            seed = seeds[seed_idx]
+            seed_idx += 1
+            if seed_idx == args.cache:
+                seed_idx = 0
+
+        heatmap[-1][MARGIN:-MARGIN] = seed
 
         chars = to_chars(np.floor(heatmap * 10))
-
         frame = "\n".join(["".join(d) for d in chars.tolist()[: -args.clip]])
+
+        if args.cache:
+            h = hash(frame)
+            if h in hashes and len(hashes) > 5:
+                cache = cache[hashes[h] + 1 :]
+                assert cache[0] == frame
+                break
+
+            cache.append(frame)
+            hashes[h] = len(hashes)
+
         await queue.put(frame)
+
+    if not cache:
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        return
+
+    while True:
+        for frame in cache:
+            await queue.put(frame)
 
 
 async def amain():
-    queue = asyncio.Queue(maxsize=5)
-    generator = asyncio.create_task(framegen(queue))
+    queue = asyncio.Queue(maxsize=args.buffer)
+    generator = asyncio.create_task(generate(queue))
 
     while True:
         frame = await queue.get()
         print(CLEAR + frame, end="")
         queue.task_done()
         await asyncio.sleep(args.timing)
+
 
 print(HIDE_CURSOR)
 try:
